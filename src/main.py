679a77f83e89d1027d4ed61e9c43ebc8574b4984
Shaan Chander <shaan.chander06@gmail.com>
2025-08-12 15:33:22 -0400

    Program now only sends newest notifications and keeps track of last sent time

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 import yaml
 from functions import *
+from datetime import datetime
+import os
 
 requiredOptions = [('insiders', 'list'), ('discordWebhookUrl', 'string'), ('excludeBoostedPosts', 'boolean')]
 
@@ -16,12 +18,40 @@ for opt in requiredOptions:
             print(f'{option[0]} ({option[1]})')
 
         exit()
+
+lastSend = ""
+if os.path.exists('../lastSend.txt'):
+    with open('../lastSend.txt', 'r') as file:
+        lastSend = file.readline()
+else:
+    lastSend = str(datetime.now())
+    print('lastSend.txt not found - initializing new one with current time')
+
+    with open('../lastSend.txt', 'w') as file:
+        file.write(str(datetime.now()))
+
+    exit()
     
 insiders = confData['insiders']
 discordWebhookUrl = confData['discordWebhookUrl']
+
+postsToSend = []
 
 for insider in insiders:
 
     posts = getPosts(insider, exclude_reblogs=confData['excludeBoostedPosts'], exclude_replies=True)
 
-    sendInsiderPost(posts[0], discordWebhookUrl)
+    for post in posts:
+        if post['created_at'] > lastSend:
+            postsToSend.append(post)
+
+    # sendInsiderPost(posts[0], discordWebhookUrl)
+
+postsToSend = sorted(postsToSend, key = lambda x:datetime.strptime(x['created_at'], '%Y-%m-%dT%H:%M:%S.%fZ'))
+
+for post in postsToSend:
+    # print(post['account']['username'])
+    sendInsiderPost(post, discordWebhookUrl)
+
+with open('../lastSend.txt', 'w') as file:
+    file.write(str(datetime.now()))
