9e52c42cbb94632811d3b99aaee835eb6f73c6b2
Shaan Chander <shaan.chander06@gmail.com>
2025-08-14 16:58:33 -0400

    Added main guard and fixed file paths (main.py can be run from outside src now)

--- src/main.py
+++ src/main.py
@@ -3,57 +3,63 @@ from functions import *
 from datetime import datetime, timezone
 import os
 
-requiredOptions = [('insiders', 'list'), ('discordWebhookUrl', 'string'), ('excludeBoostedPosts', 'boolean')]
-dateTimeFormat = '%Y-%m-%dT%H:%M:%S.%fZ'
+def main():
 
-confData= {}
+    projDir = os.path.dirname(__file__)
+    requiredOptions = [('insiders', 'list'), ('discordWebhookUrl', 'string'), ('excludeBoostedPosts', 'boolean')]
+    dateTimeFormat = '%Y-%m-%dT%H:%M:%S.%fZ'
 
-with open('../conf.yaml', 'r') as file:
-    confData = yaml.safe_load(file)
+    confData= {}
 
-for opt in requiredOptions:
-    if opt[0] not in confData:
-        print("YAML doesn't include all config options. They should include the below options:\n")
+    with open(projDir + '/../conf.yaml', 'r') as file:
+        confData = yaml.safe_load(file)
 
-        for option in requiredOptions:
-            print(f'{option[0]} ({option[1]})')
+    for opt in requiredOptions:
+        if opt[0] not in confData:
+            print("YAML doesn't include all config options. They should include the below options:\n")
+
+            for option in requiredOptions:
+                print(f'{option[0]} ({option[1]})')
+
+            exit()
+
+    lastSend = ""
+    if os.path.exists(projDir + '/../lastSend.txt'):
+        with open(projDir + '/../lastSend.txt', 'r') as file:
+            lastSend = datetime.strptime(file.readline(), dateTimeFormat)
+    else:
+        lastSend = datetime.now(timezone.utc)
+        print('lastSend.txt not found - initializing new one with current time')
+
+        with open(projDir + '/../lastSend.txt', 'w') as file:
+            file.write(datetime.now(timezone.utc).strftime(dateTimeFormat))
 
         exit()
+        
+    insiders = confData['insiders']
+    discordWebhookUrl = confData['discordWebhookUrl']
 
-lastSend = ""
-if os.path.exists('../lastSend.txt'):
-    with open('../lastSend.txt', 'r') as file:
-        lastSend = datetime.strptime(file.readline(), dateTimeFormat)
-else:
-    lastSend = datetime.now(timezone.utc)
-    print('lastSend.txt not found - initializing new one with current time')
+    postsToSend = []
 
-    with open('../lastSend.txt', 'w') as file:
+    for insider in insiders:
+
+        posts = getPosts(insider, exclude_reblogs=confData['excludeBoostedPosts'], exclude_replies=True)
+
+        for post in posts:
+            if datetime.strptime(post['created_at'], dateTimeFormat) > lastSend:
+                print(f'{post['created_at']} > {lastSend}')
+                postsToSend.append(post)
+
+        # sendInsiderPost(posts[0], discordWebhookUrl)
+
+    postsToSend = sorted(postsToSend, key = lambda x:datetime.strptime(x['created_at'], dateTimeFormat))
+
+    for post in postsToSend:
+        # print(post['account']['username'])
+        sendInsiderPost(post, discordWebhookUrl)
+
+    with open(projDir + '/../lastSend.txt', 'w') as file:
         file.write(datetime.now(timezone.utc).strftime(dateTimeFormat))
 
-    exit()
-    
-insiders = confData['insiders']
-discordWebhookUrl = confData['discordWebhookUrl']
-
-postsToSend = []
-
-for insider in insiders:
-
-    posts = getPosts(insider, exclude_reblogs=confData['excludeBoostedPosts'], exclude_replies=True)
-
-    for post in posts:
-        if datetime.strptime(post['created_at'], dateTimeFormat) > lastSend:
-            print(f'{post['created_at']} > {lastSend}')
-            postsToSend.append(post)
-
-    # sendInsiderPost(posts[0], discordWebhookUrl)
-
-postsToSend = sorted(postsToSend, key = lambda x:datetime.strptime(x['created_at'], dateTimeFormat))
-
-for post in postsToSend:
-    # print(post['account']['username'])
-    sendInsiderPost(post, discordWebhookUrl)
-
-with open('../lastSend.txt', 'w') as file:
-    file.write(datetime.now(timezone.utc).strftime(dateTimeFormat))
+if __name__ == "__main__":
+    main()
